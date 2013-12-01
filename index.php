afe6636d12b374c72702790af91cede910e6ad91
李优优 <liyouyou@shopex.cn>
2013-12-01 15:26:38 +0800

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('UTC');
 if (false !== strpos('www.jason.com', $_SERVER['SERVER_NAME'])) {
     defined('YII_DEBUG') or define('YII_DEBUG',true);
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
