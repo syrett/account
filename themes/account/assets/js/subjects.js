46bd32f7f4d9e25308755f584d7802ceb56b914e
pdwjun <pdwjun@gmail.com>
2013-12-08 23:53:51 +0800

    Subjects filter.

--- themes/account/assets/js/subjects.js
+++ themes/account/assets/js/subjects.js
@@ -2,14 +2,11 @@
  * Created by jason.wang on 13-12-6.
  */
 $(document).ready(function () {
-    $("#Subjects_sbj_name").blur(function(){
-        if ($("#Subjects_sbj_name").val()==""){
-            $("#Subjects_sbj_name").addClass("error");
-            $("#sbj_name_msg").html("科目名称 不能为空");
-        }
-        if ($("#Subjects_sbj_name").val()!=""){
-            $("#Subjects_sbj_name").removeClass("error");
-            $("#sbj_name_msg").html("");
+    $("input[name='Subjects[sbj_cat]']").keyup(function () {
+        var ob = this;
+        var invalidChars = /[^1-5]/gi
+        if (invalidChars.test(ob.value)) {
+            ob.value = ob.value.replace(invalidChars, "");
         }
     })
 });
