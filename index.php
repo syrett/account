b13215c8f81493077aa5d62b3da6b8e2595c15b9
jason.wang <pdwjun@126.com>
2013-12-16 14:43:49 +0800

    phpstorm

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ date_default_timezone_set('UTC');
 if (false !== strpos('www.jason.com', $_SERVER['SERVER_NAME'])) {
     defined('YII_DEBUG') or define('YII_DEBUG',true);
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-    $yii=dirname(__FILE__).'/vendor/yii/framework/yii.php';
+    $yii=dirname(__FILE__).'/framework/yii.php';
     $config=dirname(__FILE__).'/protected/config/development.php';
 } else {
-    $yii=dirname(__FILE__).'/vendor/yii/framework/yii.php';
+    $yii=dirname(__FILE__).'/framework/yii.php';
     $config=dirname(__FILE__).'/protected/config/production.php';
 }
 
