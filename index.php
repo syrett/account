58345bfd57b2041d57e21f60bca713f10ff49a30
jason.wang <pdwjun@126.com>
2013-12-18 18:25:36 +0800

    config

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-date_default_timezone_set('UTC');
+
 if (false !== strpos('www.jason.com', $_SERVER['SERVER_NAME'])) {
     defined('YII_DEBUG') or define('YII_DEBUG',true);
     defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-    $yii=dirname(__FILE__).'/framework/yii.php';
+    $yii=dirname(__FILE__).'/vendor/yii/framework/yii.php';
     $config=dirname(__FILE__).'/protected/config/development.php';
 } else {
-    $yii=dirname(__FILE__).'/framework/yii.php';
-    $config=dirname(__FILE__).'/protected/config/production.php';
+    $yii=dirname(__FILE__).'/vendor/yii/framework/yii.php';
+    $config=dirname(__FILE__).'/protected/config/development.php';
 }
 
 require_once($yii);
