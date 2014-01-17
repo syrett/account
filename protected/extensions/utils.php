076a5e1059d579bde0b0ac7711ef1f3aba68457f
李优优 <liyouyou@shopex.cn>
2014-01-17 17:56:20 +0800

    modify

--- protected/extensions/utils.php
+++ protected/extensions/utils.php
@@ -2,5 +2,8 @@
 
 function beTranPrefix($year, $month)
 {
-  return date("Ym", mktime(0,0,0,$month,01,$year));
+  if($year==null || $month==null)
+    return date('Y').date('m');
+  else
+    return date("Ym", mktime(0,0,0,$month,01,$year));
 }
