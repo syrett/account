b2126816f0c31f1cbb75f930521e607d17fb2878
pdwjun <pdwjun@gmail.com>
2014-02-14 19:19:11 +0800

    WebUser

---
+++ protected/components/WebUser.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sbc
+ * Date: 2/6/14
+ * Time: 5:58 PM
+ */
+
+class WebUser extends CWebUser
+{
+    /**
+     * Overrides a Yii method that is used for roles in controllers (accessRules).
+     *
+     * @param string $operation Name of the operation required (here, a role).
+     * @param mixed $params (opt) Parameters for this operation, usually the object to access.
+     * @return bool Permission granted?
+     */
+    public function checkAccess($operation, $params=array())
+    {
+        if (empty($this->id)) {
+            // Not identified => no rights
+            return false;
+        }
+        $role = $this->getState("roles");
+        if ($role === 'admin') {
+            return true; // admin role has access to everything
+        }
+        // allow access if the operation request is the current user's role
+        return ($operation === $role);
+    }
+}
