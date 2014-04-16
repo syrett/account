3aa732903351c8bf30ec0f8d9958dffabe6b054b
Michael ZHANG <geek.michael@live.com>
2014-04-16 16:32:48 +0800

    Upgraded to Bootstrap v3

---
+++ themes/abound/assets/js/profit.js
@@ -0,0 +1,28 @@
+/**
+ * Created by - on 23/02/14.
+ */
+
+$(document).ready(function () {
+
+    $('#date').datepicker({
+        dateFormat: 'yymm',
+        changeMonth: true,
+        changeYear: true,
+        showButtonPanel: true,
+
+        onClose: function(dateText, inst) {
+            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+            var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+            $(this).val($.datepicker.formatDate('yymm', new Date(year, month, 1)));
+        }
+    });
+
+    $(".monthPicker").focus(function () {
+        $(".ui-datepicker-calendar").hide();
+        $("#ui-datepicker-div").position({
+            my: "center top",
+            at: "center bottom",
+            of: $(this)
+        });
+    });
+})
