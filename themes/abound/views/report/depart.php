775fb9f91df5f1b16310e867bf662cb389ead5e6
Michael ZHANG <geek.michael@live.com>
2014-06-06 23:15:21 +0800

    Update report theme

--- themes/abound/views/report/depart.php
+++ themes/abound/views/report/depart.php
@@ -1,4 +1,4 @@
- <!-- 项目报表 -->
+<!-- 部门报表 -->
 <?php
 Yii::import('ext.select2.Select2');
 Yii::app()->clientScript->registerCoreScript('jquery');
@@ -6,29 +6,19 @@ $cs = Yii::app()->clientScript;
 $cs->registerScriptFile(Yii::app()->theme->baseUrl . '/assets/js/jquery-ui-1.10.4.custom.js', CClientScript::POS_HEAD);
 $cs->registerCssFile(Yii::app()->theme->baseUrl . '/assets/css/jquery-ui-1.10.4.custom.css');
 $cs->registerScriptFile(Yii::app()->theme->baseUrl . '/assets/js/profit.js', CClientScript::POS_HEAD);
-
 ?>
 <style>
-.table-c table{border-right:1px solid #F00;border-top:1px solid #F00; cellpadding:0; cellspacing:0 }
-.table-c table th{border-left:1px solid #F00;border-bottom:1px solid #F00; cellpadding:0; cellspacing:0}
-.table-c table td{border-left:1px solid #F00;border-bottom:1px solid #F00; cellpadding:0; cellspacing:0}
 .ui-datepicker table{
     display: none;
 }
 </style>
 
-<style>
-.table-d table{ background:#000; border-right:1px solid #000}
-.table-d table td{ background:#FFF}
-</style>
-
 <?php
-function echoData($data, $subjects, $options=array("css"=>"table-c"))
+function echoData($data, $subjects)
 {
-
   $column = array();
     echo "<tr>";
-    echo "<th > </th>";
+    echo "<th>&nbsp;</th>";
   foreach($subjects as $sbj_id=>$sbj_name){
     echo "<th>".$sbj_name."</th>";
     $column[]=$sbj_id;
@@ -36,20 +26,11 @@ function echoData($data, $subjects, $options=array("css"=>"table-c"))
   echo "</tr>";
 
   $column_len = count($column);
-  if (empty($options["css"]))
-    {
-      $css= "table-c";
-    }
-  else
-    {
-    $css = $options["css"];
-    }   
-
+ 
     foreach($data as $k=>$depart)
       {
 
         echo "<tr>";
-        echo "<div class=".$css.">";
         echo "<td>".$depart["name"]."</td>";
         for($i=0;$i<$column_len;$i++){
           if(isset($depart[$column[$i]])){
@@ -59,23 +40,17 @@ function echoData($data, $subjects, $options=array("css"=>"table-c"))
           }
         echo "<td>".number_format($balance, 2)."</td>";
         }
-        echo "</div>";
         echo "</tr>";                                                   
-
       }
 }
 ?>
-
-<div class="table-c";>
-
- <div>
-<?php echo CHtml::beginForm(); ?>
-<h5>日期:
-    <input type="text" name="date" id="date" class="span2" value="<?php echo isset($date)?$date:'' ?>" readonly/>
-</h5>
-
-    <h5>
-        选择科目
+<div class="alert alert-info">
+	<?php echo CHtml::beginForm('','post',array('class'=>'form-inline')); ?>
+	<h3>部 门 表</h3>
+	<div class="form-group">
+		<label class="control-label" for="date">请选择报表日期：</label>
+		<input class="form-control" type="text" name="date" id="date" value="<?php echo isset($date)?$date:'' ?>" readonly />
+        <label class="control-label" for="sbj_id">选择科目</label>
         <?php
         $this->widget('Select2', array(
             'name' => 'sbj_id',
@@ -83,28 +58,19 @@ function echoData($data, $subjects, $options=array("css"=>"table-c"))
             'data' => $list,
         ));
         ?>
-    </h5>
-
-<input type="submit" value="查看报表" />
-<?php echo CHtml::endForm(); ?>
-    </div>
-<div style="display:<?php if($data=='') echo 'none';?>">
-<table cellpadding="0" cellspacing="0" style="padding:0px;margin:0px;">
-                                         <tr>
-                                         <td colspan=2 align=right> 金额单位:元 </td>
-                                         </tr>
-
-    <?php echoData($data, $subjects) ?>
-
-                                         </td>
-                                         </table>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
+		<input type="submit" class="btn btn-primary" value="查看报表" />
+	</div>
+	<p>&nbsp;</p>
+	<?php echo CHtml::endForm(); ?>
+</div>
+<div <?php if($data=='') echo 'style="display:none"'; ?>" class="panel panel-default">
+	<div class="panel-heading">
+		<h2>部 门 表</h2>
+	</div>
+	<div class="panel-body">
+		<p class="pull-right">金额单位：元</p>
+	</div>
+	<table class="table table-bordered table-hover">
+		<?php echoData($data, $subjects) ?>
+    </table>
+</div>
