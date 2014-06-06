58254ec9dace8e4c43e4bf5a113bf11a77436052
Michael ZHANG <geek.michael@live.com>
2014-06-06 22:56:26 +0800

    Update with Bootstrap theme

--- themes/abound/views/report/client.php
+++ themes/abound/views/report/client.php
@@ -9,36 +9,17 @@ $cs->registerScriptFile(Yii::app()->theme->baseUrl . '/assets/js/profit.js', CCl
 
 ?>
 <style>
-.table-c table{border-right:1px solid #F00;border-top:1px solid #F00; cellpadding:0; cellspacing:0 }
-.table-c table th{border-left:1px solid #F00;border-bottom:1px solid #F00; cellpadding:0; cellspacing:0}
-.table-c table td{border-left:1px solid #F00;border-bottom:1px solid #F00; cellpadding:0; cellspacing:0}
 .ui-datepicker table{
     display: none;
 }
 </style>
 
-<style>
-.table-d table{ background:#000; border-right:1px solid #000}
-.table-d table td{ background:#FFF}
-</style>
-
 <?php
-function echoData($data, $options=array("css"=>"table-c"))
+function echoData($data)
 {
-
-  if (empty($options["css"]))
-    {
-      $css= "table-c";
-    }
-  else
-    {
-    $css = $options["css"];
-    }   
-
-    foreach($data as $ti)
+   foreach($data as $ti)
       {
         echo "<tr>";
-        echo "<div class=".$css.">";
         echo "<td>".$ti["company"]."</td>";
         echo "<td>".number_format($ti["month_debit"], 2)."</td>";
         echo "<td>".number_format($ti["month_credit"], 2)."</td>";
@@ -50,36 +31,34 @@ function echoData($data, $options=array("css"=>"table-c"))
 }
 ?>
 
-<div class="table-c";>
+<div class="alert alert-info">
+	<?php echo CHtml::beginForm('','post',array('class'=>'form-inline')); ?>
+	<h3>客 户 表</h3>
+	<div class="form-group">
+		<label class="control-label" for="date">请选择报表日期：</label>
+		<input class="form-control" type="text" name="date" id="date" value="<?php echo isset($date)?$date:'' ?>" readonly />
+		<input type="submit" class="btn btn-primary" value="查看报表" />
+	</div>
+	<?php echo CHtml::endForm(); ?>
+	<p>&nbsp;</p>
+</div>
 
- <div>
-<?php echo CHtml::beginForm(); ?>
-<h5>日期:
-    <input type="text" name="date" id="date" class="span2" value="<?php echo isset($date)?$date:'' ?>" readonly/>
-</h5>
-
-<input type="submit" value="查看报表" />
-<?php echo CHtml::endForm(); ?>
-    </div>
-<div style="display:<?php if($data=='') echo 'none';?>">
-<table cellpadding="0" cellspacing="0" style="padding:0px;margin:0px;">
-                                         <tr>
-                                         <td colspan=6 align=right> 金额单位:元 </td>
-                                         </tr>
-
-                                         <tr>
-                                         <th > </th>
-                                         <th >本期借方</th>
-                                         <th>本期贷方</th>
-                                         <th >本年借方</th>
-                                         <th>本年贷方</th>
-                                         <th >余额</th>
-                                         </tr>
-
-
-    <?php echoData($data) ?>
-
-                                         </td>
-                                         </table>
-    </div>
-  </div>
+<div <?php if(!$data) echo 'style="display:none"'; ?>" class="panel panel-default">
+	<div class="panel-heading">
+		<h2>客 户 表</h2>
+	</div>
+	<div class="panel-body">
+		<p class="pull-right">金额单位：元</p>
+	</div>
+	<table class="table table-bordered table-hover">
+	 <tr>
+	 <th>&nbsp;</th>
+	 <th>本期借方</th>
+	 <th>本期贷方</th>
+	 <th>本年借方</th>
+	 <th>本年贷方</th>
+	 <th>余额</th>
+	 </tr>
+     <?php echoData($data) ?>
+	 </table>
+</div>
