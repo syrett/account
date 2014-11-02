d1ae96f46acfc4570680f7af0c2f6a1529d61938
Michael ZHANG <geek.michael@live.com>
2014-11-02 19:00:20 +0800

    根据二级域名自动调用公司账套功能

--- protected/config/development.php
+++ protected/config/development.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'Wojia',
+    'name'=>'Sorcerer_Account',
 //    'theme'=>'account',
     'theme'=>'abound',
     // preloading 'log' component
--- protected/config/main.php
+++ protected/config/main.php
@@ -7,8 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-	'theme'=>'account',
+	'name'=>'Sorcerer_Account',
+	'theme'=>'abound',
 //    'theme'=>'blackboot',
 	// preloading 'log' component
 	'preload'=>array('log'),
