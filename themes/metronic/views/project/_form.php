af540b77a9527501068c409f1b6d994e1691d915
Michael ZHANG <geek.michael@live.com>
2015-06-26 22:53:24 +0800

    Update project management pages

--- themes/metronic/views/project/_form.php
+++ themes/metronic/views/project/_form.php
@@ -8,18 +8,7 @@ $form=$this->beginWidget('CActiveForm', array(
 	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
 	'htmlOptions'=>array('class'=>'form-horizontal',),
-)); ?>
-	<div class="well well-sm">
-	<?php
-	echo CHtml::link('<span class="glyphicon glyphicon-search"></span> 项目列表',array('admin'),array('class' => 'btn btn-default'));
-	if (!($model->isNewRecord)) {
-	echo "\n";
-	echo CHtml::link('<span class="glyphicon glyphicon-plus"></span> 添加项目',array('create'),array('class' => 'btn btn-default'));
-	}
-	?>
-	</div>
-	
-	<div class="panel-body">
+)); ?>	
 		<div class="form-group">
 		<?php echo $form->labelEx($model,'name',array('class'=>'col-sm-2 control-label')); ?>
 		<div class="col-sm-10">
@@ -36,9 +25,8 @@ $form=$this->beginWidget('CActiveForm', array(
 		</div>
 		<div class="form-group">
 		<div class="col-sm-offset-2 col-sm-10 text-center">
-			<?php echo CHtml::submitButton('保存', array('class'=>'btn btn-primary',)); ?>
+			<?php echo CHtml::submitButton('保存', array('class'=>'btn btn-circle btn-primary',)); ?>
 			<?php echo BtnBack(); ?>
 		</div>
 		</div>
-	</div>
 <?php $this->endWidget(); ?>
--- themes/metronic/views/project/admin.php
+++ themes/metronic/views/project/admin.php
@@ -6,18 +6,19 @@ $this->breadcrumbs=array(
 	'项目管理',
 );
 ?>
-<div class="panel panel-default voucher form">
-    <!-- Default panel contents -->
-    <div class="panel-heading">
-		<h2>项目管理</h2>
+<div class="portlet light">
+	<div class="portlet-title">
+		<div class="caption">
+		<span class="font-green-sharp">项目管理</span>
+		</div>
+		<div class="actions">
+		    <?php
+				echo CHtml::link('<i class="fa fa-plus"></i> 添加项目', array('create'), array('class' => 'btn btn-circle btn-primary btn-sm'));
+    		?>
+			<a href="javascript:;" class="btn btn-circle btn-default btn-icon-only fullscreen" data-original-title="" data-original-title title="全屏"></a>
+		</div>
 	</div>
-
-	<div class="well well-sm">
-	<?php
-	echo CHtml::link('<span class="glyphicon glyphicon-plus"></span> 添加',array('create'),array('class' => 'btn btn-default'));
-	?>
-	</div>
-    <div class="panel-body">
+	<div class="portlet-body">
 
 <?php
 $this->widget('zii.widgets.grid.CGridView', array(
