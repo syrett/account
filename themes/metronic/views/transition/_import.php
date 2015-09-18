4a8e6a5c87b5dcdf7c05e935161e0515ea265935
pdwjun@gmail.com <ulh5558290555>
2015-09-18 10:17:31 +0800

    银行UI

--- themes/metronic/views/transition/_import.php
+++ themes/metronic/views/transition/_import.php
@@ -43,7 +43,7 @@ $tranDate = $this->getTransitionDate('post');
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-12">
+            <div class="col-md-3 col-sm-12">
                 <div class="btn-toolbar margin-bottom-10">
                     <button type="submit" class="btn btn-default btn-file">导入</button>
                     <button download="" href="/download/<?= Yii::t('import', strtoupper($type)) ?>.xlsx"
@@ -52,7 +52,7 @@ $tranDate = $this->getTransitionDate('post');
                 </div>
 
             </div>
-            <div class="col-md-4 col-sm-12">
+            <div class="col-md-5 col-sm-12">
                 <div class="form-inline pull-right">
                     <div class="form-group">
                         <?php
