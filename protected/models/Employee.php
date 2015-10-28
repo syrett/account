71ff5556f8bcca99e97b6b8a6bd14785c0e27664
pdwjun@gmail.com <ulh5558290555>
2015-10-28 09:37:08 +0800

    工资个税公式bug

--- protected/models/Employee.php
+++ protected/models/Employee.php
@@ -159,9 +159,9 @@ class Employee extends CActiveRecord
             return 0;
         switch($amount){
             case $amount <= 1500 : $tax = 3; $base = 0;break;
-            case $amount > 1500 && $amount <= 4500 : $tax = 3; $base = 105;break;
-            case $amount > 4500 && $amount <= 9000 : $tax = 10; $base = 555;break;
-            case $amount > 9000 && $amount <= 35000 : $tax = 20; $base = 1005;break;
+            case $amount > 1500 && $amount <= 4500 : $tax = 10; $base = 105;break;
+            case $amount > 4500 && $amount <= 9000 : $tax = 20; $base = 555;break;
+            case $amount > 9000 && $amount <= 35000 : $tax = 25; $base = 1005;break;
             case $amount > 35000 && $amount <= 55000 : $tax = 30; $base = 2755;break;
             case $amount > 55000 && $amount <= 80000 : $tax = 35; $base = 5505;break;
             case $amount > 80000 : $tax = 45; $base = 13505;break;
