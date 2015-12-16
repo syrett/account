fbb2d6bedd11e6828548f13d68fa5ccd304af824
pdwjun@gmail.com <ulh5558290555>
2015-12-16 10:38:37 +0800

    成本结转delete请求参数400bug

--- protected/controllers/CostController.php
+++ protected/controllers/CostController.php
@@ -15,7 +15,6 @@ class CostController extends Controller
 	{
 		return array(
 			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
 		);
 	}
 
@@ -140,7 +139,7 @@ class CostController extends Controller
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('index'));
 	}
 
 	/**
