ef8ae4a7b2da1fa23b97c4990e280a24f9c16458
pdwjun <pdwjun@gmail.com>
2016-03-03 13:46:17 +0800

    提交message,给林强做翻译工作

--- themes/metronic/views/report/balance.php
+++ themes/metronic/views/report/balance.php
@@ -45,177 +45,177 @@ function echoData($key, $data, $name="default")
 ?>
 <div class="alert alert-info">
         <?php echo CHtml::beginForm('','post',array('class'=>'form-inline')); ?>
-        <h3><?= Yii::t('import', '资产负债表') ?></h3>
+        <h3><?= Yii::t('report', '资产负债表') ?></h3>
         <div class="form-group">
-            <label class="control-label" for="date"><?= Yii::t('import', '请选择报表日期：') ?></label>
+            <label class="control-label" for="date"><?= Yii::t('report', '请选择报表日期') ?>:</label>
 			<input type="text" data-date-format="yyyymmdd" name="date" class="form-control form-control-inline input-small date-picker" value="<?php echo isset($date)?$date:'' ?>" id="date" readonly="">
-            <input type="submit" class="btn btn-primary" value="<?= Yii::t('import', '查看报表') ?>" />
+            <input type="submit" class="btn btn-primary" value="<?= Yii::t('report', '查看报表') ?>" />
         </div>
         <p>&nbsp;</p>
         <?php echo CHtml::endForm(); ?>
 </div>
 <div <?php if(!$data) echo 'style="display:none"'; ?> class="panel panel-default">
         <div class="panel-heading">
-                <h2><?= Yii::t('import', '资 产 负 债 表') ?></h2>
+                <h2><?= Yii::t('report', '资 产 负 债 表') ?></h2>
         </div>
         <div class="panel-body">
-                <p class="text-center"><span class="pull-left"><?= Yii::t('import', '日期：') ?><?php echo date('Y-m-d',strtotime($date)); ?></span> <?= Yii::t('import', '编制单位：') ?><?php echo $company ?> <span class="pull-right"><?= Yii::t('import', '金额单位：元') ?></span></p>
+                <p class="text-center"><span class="pull-left"><?= Yii::t('report', '日期') ?>: <?php echo date('Y-m-d',strtotime($date)); ?></span> <?= Yii::t('report', '编制单位') ?>: <?php echo $company ?> <span class="pull-right"><?= Yii::t('report', '金额单位：元') ?></span></p>
         </div>
 
         <table class="table table-bordered table-hover" id="balance">
                 <thead>
                  <tr>
-                 <th><?= Yii::t('import', '资产') ?></th>
-                 <th class="text-right"><?= Yii::t('import', '年初数') ?></th>
-                 <th class="text-right"><?= Yii::t('import', '期末数') ?></th>
-                 <th><?= Yii::t('import', '负债及股东权益(所有者权益)') ?></th>
-                 <th class="text-right"><?= Yii::t('import', '年初数') ?></th>
-                 <th class="text-right"><?= Yii::t('import', '期末数') ?></th>
+                 <th><?= Yii::t('report', '资产') ?></th>
+                 <th class="text-right"><?= Yii::t('report', '年初数') ?></th>
+                 <th class="text-right"><?= Yii::t('report', '期末数') ?></th>
+                 <th><?= Yii::t('report', '负债及股东权益(所有者权益)') ?></th>
+                 <th class="text-right"><?= Yii::t('report', '年初数') ?></th>
+                 <th class="text-right"><?= Yii::t('report', '期末数') ?></th>
                  </tr>
                 </thead>
                  <tr>
-                 <?php echoData(0, $data, Yii::t('import', "流动资产:")) ?>
-                 <?php echoData(0, $data,  Yii::t('import', "流动负债:")) ?>
+                 <?php echoData(0, $data, Yii::t('report', "流动资产")) ?>:
+                 <?php echoData(0, $data,  Yii::t('report', "流动负债")) ?>:
                  </tr>
                  <tr>
-                 <?php echoData(1, $data, Yii::t('import', "货币资金" )) ?>
-                 <?php echoData(31, $data,  Yii::t('import', "短期借款")) ?>
+                 <?php echoData(1, $data, Yii::t('report', "货币资金" )) ?>
+                 <?php echoData(31, $data,  Yii::t('report', "短期借款")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(2, $data,  Yii::t('import', "交易性金融资产")) ?>
-                 <?php echoData(32, $data,  Yii::t('import', "交易性金融负债")) ?>
+                 <?php echoData(2, $data,  Yii::t('report', "交易性金融资产")) ?>
+                 <?php echoData(32, $data,  Yii::t('report', "交易性金融负债")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(3, $data,  Yii::t('import', "应收票据")) ?>
-                 <?php echoData(33, $data,  Yii::t('import', "应付票据")) ?>
+                 <?php echoData(3, $data,  Yii::t('report', "应收票据")) ?>
+                 <?php echoData(33, $data,  Yii::t('report', "应付票据")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(4, $data,  Yii::t('import', "应收账款")) ?>
+                 <?php echoData(4, $data,  Yii::t('report', "应收账款")) ?>
                  <?php echoData(34, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(9, $data,  Yii::t('import', "减:坏账准备")) ?>
+                 <?php echoData(9, $data,  Yii::t('report', "减:坏账准备")) ?>
                  <?php echoData(35, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(5, $data,  Yii::t('import', "预付账款")) ?>
+                 <?php echoData(5, $data,  Yii::t('report', "预付账款")) ?>
                  <?php echoData(36, $data ) ?>
                  </tr>
                  <tr>
-                 <?php echoData(8, $data,  Yii::t('import', "其他应收款")) ?>
+                 <?php echoData(8, $data,  Yii::t('report', "其他应收款")) ?>
                  <?php echoData(37, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(11, $data,  Yii::t('import', "存货")) ?>
+                 <?php echoData(11, $data,  Yii::t('report', "存货")) ?>
                  <?php echoData(39, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(12, $data,  Yii::t('import', "减:存货跌价准备")) ?>
+                 <?php echoData(12, $data,  Yii::t('report', "减:存货跌价准备")) ?>
                  <?php echoData(40, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(0, $data,  Yii::t('import', "一年内到期的非流动资产")) ?>
-                 <?php echoData(0, $data,  Yii::t('import', "一年内到期的非流动负债 ")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "一年内到期的非流动资产")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "一年内到期的非流动负债 ")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(10, $data,  Yii::t('import', "其他流动资产")) ?>
+                 <?php echoData(10, $data,  Yii::t('report', "其他流动资产")) ?>
                  <?php echoData(41, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData("flow_property", $data,  Yii::t('import', "流动资产合计")) ?>
-                 <?php echoData("flow_debt", $data,  Yii::t('import', "流动负债合计")) ?>
+                 <?php echoData("flow_property", $data,  Yii::t('report', "流动资产合计")) ?>
+                 <?php echoData("flow_debt", $data,  Yii::t('report', "流动负债合计")) ?>
                  </tr>
                  <tr>
-                 <?php echoData("empty", $data,  Yii::t('import', "非流动资产:")) ?>
+                 <?php echoData("empty", $data,  Yii::t('report', "非流动资产:")) ?>
                  <?php echoData("empty", $data,  "") ?>
                  </tr>
                  <tr>
-                 <?php echoData(15, $data,  Yii::t('import', "可供出售金融资产")) ?>
-                 <?php echoData(0, $data,  Yii::t('import', "非流动负债:")) ?>
+                 <?php echoData(15, $data,  Yii::t('report', "可供出售金融资产")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "非流动负债")) ?>:
                  </tr>
                  <tr>
-                 <?php echoData(0, $data,  Yii::t('import', "减:可供出售金融资产减值准备")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "减:可供出售金融资产减值准备")) ?>
                  <?php echoData(42, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(13, $data,  Yii::t('import', "持有至到期投资")) ?>
+                 <?php echoData(13, $data,  Yii::t('report', "持有至到期投资")) ?>
                  <?php echoData(43, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(14, $data,  Yii::t('import', "减:持有至到期投资减值准备")) ?>
+                 <?php echoData(14, $data,  Yii::t('report', "减:持有至到期投资减值准备")) ?>
                  <?php echoData(45, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(19, $data,  Yii::t('import', "长期应收款")) ?>
+                 <?php echoData(19, $data,  Yii::t('report', "长期应收款")) ?>
                  <?php echoData(46, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(16, $data,  Yii::t('import', "长期股权投资")) ?>
+                 <?php echoData(16, $data,  Yii::t('report', "长期股权投资")) ?>
                  <?php echoData(47, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(17, $data,  Yii::t('import', "减:长期股权投资减值准备")) ?>
+                 <?php echoData(17, $data,  Yii::t('report', "减:长期股权投资减值准备")) ?>
                  <?php echoData(48, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(18, $data,  Yii::t('import', "投资性房地产")) ?>
+                 <?php echoData(18, $data,  Yii::t('report', "投资性房地产")) ?>
                  <?php echoData(44, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(0, $data, Yii::t('import', "减:投资性房地产减值准备")) ?>
-                 <?php echoData("unflow_debt",$data, Yii::t('import', "非流动负债合计")) ?>
+                 <?php echoData(0, $data, Yii::t('report', "减:投资性房地产减值准备")) ?>
+                 <?php echoData("unflow_debt",$data, Yii::t('report', "非流动负债合计")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(21, $data, Yii::t('import', "固定资产")) ?>
-                 <?php echoData("debt", $data, Yii::t('import', "负债合计")) ?>
+                 <?php echoData(21, $data, Yii::t('report', "固定资产")) ?>
+                 <?php echoData("debt", $data, Yii::t('report', "负债合计")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(22, $data, Yii::t('import', "减:累计折旧")) ?>
-                 <?php echoData(0, $data, Yii::t('import', "上级拨入")) ?>
+                 <?php echoData(22, $data, Yii::t('report', "减:累计折旧")) ?>
+                 <?php echoData(0, $data, Yii::t('report', "上级拨入")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(23, $data,  Yii::t('import', "减:固定资产减值准备")) ?>
-                 <?php echoData(0, $data,  Yii::t('import', "股东权益(所有者权益):")) ?>
+                 <?php echoData(23, $data,  Yii::t('report', "减:固定资产减值准备")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "股东权益(所有者权益)")) ?>:
                  </tr>
                  <tr>
-                 <?php echoData(24, $data,  Yii::t('import', "在建工程")) ?>
+                 <?php echoData(24, $data,  Yii::t('report', "在建工程")) ?>
                  <?php echoData(50, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(0, $data,  Yii::t('import', "固定资产清理")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "固定资产清理")) ?>
                  <?php echoData(51, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(26,  $data, Yii::t('import', "无形资产")) ?>
+                 <?php echoData(26,  $data, Yii::t('report', "无形资产")) ?>
                  <?php echoData(52, $data) ?>
                  </tr>
                  <tr>
-                 <?php echoData(28, $data,  Yii::t('import', "商誉")) ?>
-                 <?php echoData("undistributed_profit", $data, Yii::t('import', "未分配利润")) ?>
+                 <?php echoData(28, $data,  Yii::t('report', "商誉")) ?>
+                 <?php echoData("undistributed_profit", $data, Yii::t('report', "未分配利润")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(29, $data,  Yii::t('import', "长期待摊费用")) ?>
-                 <?php echoData("parent_owner", $data,  Yii::t('import', "归属于母公司股东权益(所有者权益)合计")) ?>
+                 <?php echoData(29, $data,  Yii::t('report', "长期待摊费用")) ?>
+                 <?php echoData("parent_owner", $data,  Yii::t('report', "归属于母公司股东权益(所有者权益)合计")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(30, $data,  Yii::t('import', "递延所得税资产")) ?>
-                 <?php echoData(0, $data,  Yii::t('import', "少数股东权益")) ?>
+                 <?php echoData(30, $data,  Yii::t('report', "递延所得税资产")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "少数股东权益")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(20, $data,  Yii::t('import', "其他非流动资产")) ?>
+                 <?php echoData(20, $data,  Yii::t('report', "其他非流动资产")) ?>
                  <?php echoData("empty", $data,  "") ?>
                  </tr>
                  <tr>
-                 <?php echoData("unflow_property", $data,  Yii::t('import', "非流动资产合计")) ?>
-                 <?php echoData("owner", $data,  Yii::t('import', "股东权益(所有者权益)合计")) ?>
+                 <?php echoData("unflow_property", $data,  Yii::t('report', "非流动资产合计")) ?>
+                 <?php echoData("owner", $data,  Yii::t('report', "股东权益(所有者权益)合计")) ?>
                  </tr>
                  <tr>
-                 <?php echoData(0, $data,  Yii::t('import', "拨付所属资金")) ?>
+                 <?php echoData(0, $data,  Yii::t('report', "拨付所属资金")) ?>
                  <?php echoData("empty", $data,  "") ?>
                  </tr>
                  <tr>
-                 <?php echoData("property", $data,  Yii::t('import', "资产合计")) ?>
-                 <?php echoData("debt_owner", $data,  Yii::t('import', "负债及股东权益(所有者权益)合计")) ?>
+                 <?php echoData("property", $data,  Yii::t('report', "资产合计")) ?>
+                 <?php echoData("debt_owner", $data,  Yii::t('report', "负债及股东权益(所有者权益)合计")) ?>
                  </tr>
         </table>
 </div>
@@ -225,7 +225,7 @@ function echoData($key, $data, $name="default")
         echo CHtml::beginForm($this->createUrl('/Report/createexcel'), 'post');
         if ($date != ""){
                 $d = date('Y-m',strtotime($date));
-                $excel_name = Yii::t('import', "资产负债表-").$d.".xls";
+                $excel_name = Yii::t('report', "资产负债表")."-$d.xls";
                 ?>
                 <input type="hidden" name="data" id="data" value="" />
                 <input type="hidden" name="name" id="name" value="<?=$excel_name?>" />
