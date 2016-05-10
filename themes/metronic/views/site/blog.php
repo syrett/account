011a8883bc51101dc295221216d8eb21d8068d65
pdwjun@gmail.com <ulh5558290555>
2016-05-10 11:48:17 +0800

    样式

--- themes/metronic/views/site/blog.php
+++ themes/metronic/views/site/blog.php
@@ -1,9 +1,21 @@
 <style>
- a {
-     color: gray;
- }
+a {
+    color: gray;
+}
+
 a:hover {
     color: red;
+}
+
+#article-div {
+    background-color: #ffffff;
+    margin-top: 1em;
+    padding: .5em 2em 2em;
+    min-height: 560px;
+}
+
+#article-content > p {
+    line-height: 2em;
 }
 </style>
 <div>
@@ -20,21 +32,22 @@ a:hover {
     <span style="margin-left: .5em;">正文</span>
 </div>
 
-<div class="" style="margin: 1em 0;">
-    <div class="">
-        <h3><?= $article->title ?></h3>
-        <div class="text-muted">
-            <?= date('Y-m-d H:i:s', $article->created_at); ?>
-            <span class="glyphicon glyphicon-eye-open" title="浏览" style="margin-left: 2em;"></span>&nbsp;<?=$article->views;?>
+<div id="article-div">
+    <div class="" style="margin: 1em 0;">
+        <div class="">
+            <h3><?= $article->title ?></h3>
+            <div class="text-muted">
+                <?= date('Y-m-d H:i:s', $article->created_at); ?>
+                <span class="glyphicon glyphicon-eye-open" title="浏览" style="margin-left: 2em;"></span>&nbsp;<?=$article->views;?>
+            </div>
         </div>
     </div>
-</div>
-<?php if ($article->snippet != '') { ?>
-    <div class="alert alert-warning">
-        <?php echo $article->snippet; ?>
+    <?php if ($article->snippet != '') { ?>
+        <div class="alert alert-warning">
+            <?php echo $article->snippet; ?>
+        </div>
+    <?php } ?>
+    <div id="article-content" style="text-indent: 2em;">
+        <?= $article->content; ?>
     </div>
-<?php } ?>
-
-<div style="text-indent: 2em;">
-    <?= $article->content; ?>
 </div>
